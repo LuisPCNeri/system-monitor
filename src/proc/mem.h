a8cfe350015d47f419ede2a1a154a7aab2a78ef8
LuisPCNeri <luispedrocneri@gmail.com>
2026-08-16 20:31:33 +0100

    placeholder files for everything else not yet done

---
+++ src/proc/mem.h
@@ -0,0 +1,20 @@
+#ifndef MEM_H
+#define MEM_H
+
+typedef struct mem_info_t {
+    long total_kb;
+    long used_kb;
+    long total_mib;
+    long used_mib;
+} mem_info_t;
+
+/*
+ * \brief Reads MemTotal and MemAvailable from /proc/meminfo
+ * \param out Pointer to address to store the mem info.
+ * \returns 0 on success -1 on failure.
+ * */
+int mem_read(mem_info_t* out);
+
+#endif
+
+
